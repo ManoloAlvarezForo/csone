4baf2a16579224e821fb813dbad229fd56817e10
Manolo Alvarez Foro <af.manolo@gmail.com>
2017-07-26 12:13:11 -0400

    Second main push
    Added the angular material material icons dependencies and the login component this is empty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule, OverlayContainer } from "@angular/material";
+import { MdInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    MdInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
